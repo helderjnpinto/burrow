4780875be994e882c9fbda2112fc2f53dc037888
Helder Pinto <hp_ctt@hotmail.com>
2018-12-10 11:09:55 +0000

    wip original development with all permitions

---
+++ chain_dir/generate-accounts.json.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $script_dir
+
+address_of() {
+    jq -r ".Accounts | map(select(.Name == \"$1\"))[0].Address" genesis.json
+}
+
+full_addr=$(address_of "Full_0")
+
+burrow keys export --addr ${full_addr} '--template={address: "<< .Address >>", pubKey: "<< hex .PublicKey  >>", privKey: "<< hex .PrivateKey >>" }'
